b777ccdb7b0a6f305b53548401c785562780b581
Tu Nombre <tu-email@ejemplo.com>
2026-04-10 17:00:48 +0200

    David: Producto 2 - Firebase configurado y servicio creado

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey:"AIzaSyALyXa58KOKGjVm86AxfRmEHsGPiXjlVwU",
+    authDomain: "equipo-basket-producto2-caf1c.firebaseapp.com",
+    projectId: "equipo-basket-producto2-caf1c",
+    storageBucket: "equipo-basket-producto2-caf1c.firebasestorage.app",
+    messagingSenderId: "977467703158",
+    appId: "1:977467703158:web:401a2073ccae17b04dc457",
+  }
+};
